97d3338b64bca4a889f08b7e86fed82ef6409bc4
Christian-Windows <christian.winwood@aggiemail.usu.edu>
2020-05-25 15:09:32 -0600

    Ability to select any subrace

--- src/main/resources/static/characterCreation/create.js
+++ src/main/resources/static/characterCreation/create.js
@@ -8,14 +8,43 @@ function updateSubRace(){
     var selected = e.options[e.selectedIndex].value;
     
     switch(selected){
-        case "None":
+        case null:
             $("#subrace").empty();
             sub.setAttribute("disabled", "disabled");
             break;
         case "Dwarf":
+            if(sub.hasAttribute("disabled")){
+                sub.removeAttribute("disabled");
+            }
             var subs = ["Hill Dwarf", "Mountain Dwarf"];
             addItemsToSelect("subrace", subs);
             break;
+        case "Elf":
+            if(sub.hasAttribute("disabled")){
+                sub.removeAttribute("disabled");
+            }
+            var subs = ["High Elf", "Wood Elf", "Dark Elf"];
+            addItemsToSelect("subrace", subs);
+            break;
+        case "Halfling":
+            if(sub.hasAttribute("disabled")){
+                sub.removeAttribute("disabled");
+            }
+            var subs = ["Lightfoot", "Stout"];
+            addItemsToSelect("subrace", subs);
+            break;
+        case "Gnome":
+            if(sub.hasAttribute("disabled")){
+                sub.removeAttribute("disabled");
+            }
+            var subs = ["Forest Gnome", "Rock Gnome"];
+            addItemsToSelect("subrace", subs);
+            break;
+        case "Tiefling":
+        case "Half-Orc":
+        case "Half-Elf":
+        case "Dragonborn":
+        case "Human":
         default:
             $("#subrace").empty();
             if(!sub.hasAttribute("disabled")){
@@ -25,12 +54,10 @@ function updateSubRace(){
 }
 
 function addItemsToSelect(id, arr){
-    if(sub.hasAttribute("disabled")){
-        sub.removeAttribute("disabled");
-    }
 
     $("#" + id).empty();
     var select = document.getElementById(id);
+    select.options[0] = new Option("--Select An Option--", null);
     for(i in arr){
         select.options[select.options.length] = new Option(arr[i], arr[i]);
     }
