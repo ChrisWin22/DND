feabf28630fc69d0fb583b682a5aaa7e5e2a89bc
Christian-Windows <christian.winwood@aggiemail.usu.edu>
2020-05-25 12:53:57 -0600

    added functionality to select subrace

---
+++ src/main/resources/static/characterCreation/create.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    document.getElementById("race").addEventListener("change", updateSubRace);
+});
+
+function updateSubRace(){
+    var sub = document.getElementById("subrace");
+    var e = document.getElementById("race");
+    var selected = e.options[e.selectedIndex].value;
+    
+    switch(selected){
+        case "None":
+            $("#subrace").empty();
+            sub.setAttribute("disabled", "disabled");
+            break;
+        case "Dwarf":
+            if(sub.hasAttribute("disabled")){
+                sub.removeAttribute("disabled");
+            }
+
+            var subs = ["Hill Dwarf", "Mountain Dwarf"];
+
+            addItemsToSelect("subrace", subs);
+            break;
+        default:
+            $("#subrace").empty();
+            if(!sub.hasAttribute("disabled")){
+                sub.setAttribute("disabled", "disabled");
+            }
+    }
+}
+
+function addItemsToSelect(id, arr){
+    $("#" + id).empty();
+    var select = document.getElementById(id);
+    for(i in arr){
+        select.options[select.options.length] = new Option(arr[i], arr[i]);
+    }
+}
