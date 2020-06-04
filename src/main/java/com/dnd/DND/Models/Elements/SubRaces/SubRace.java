9bdb12039a235e27b204d6d6775535c06eefb261
Christian-Windows <christian.winwood@aggiemail.usu.edu>
2020-06-03 20:24:49 -0600

    see previous commit

---
+++ src/main/java/com/dnd/DND/Models/Elements/SubRaces/SubRace.java
@@ -0,0 +1,10 @@
+package com.dnd.DND.Models.Elements.SubRaces;
+
+import com.dnd.DND.Models.Elements.Races.Race;
+
+public class SubRace extends Race{
+    
+    public SubRace(){
+        super();
+    }
+}
---
+++ src/main/java/com/dnd/DND/Models/Enums/SubRaceEnum.java
@@ -0,0 +1,13 @@
+package com.dnd.DND.Models.Enums;
+
+public enum SubRaceEnum {
+    HILLDWARF,
+    MOUNTAINDWARF,
+    HIGHELF,
+    WOODELF,
+    DROW,
+    LIGHTFOOT,
+    STOUT,
+    FORESTGNOME,
+    ROCKGNOME
+}
